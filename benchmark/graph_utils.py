f03cdd1c748e3f9a985a4440dc531e8392b01a94
NicoStellwag <nico.stellwag@gmail.com>
2026-02-04 14:18:22 +0100

    make coord system metric in cm

--- benchmark/graph_utils.py
+++ benchmark/graph_utils.py
@@ -14,20 +14,35 @@ def get_coord_transformations(positions: np.ndarray):
         - distance_normalized2original: Callable(normalized_distance: float) -> original_distance: float
     """
     assert positions.shape[-1] == 3, "last dim must be 3"
-    flat_pos = positions.reshape(-1, 3)
-    mean_pos = flat_pos.mean(axis=0)
-    std_pos = flat_pos.std(axis=0) + 1e-8
+    # flat_pos = positions.reshape(-1, 3)
+    # mean_pos = flat_pos.mean(axis=0)
+    # std_pos = flat_pos.std(axis=0) + 1e-8
 
+    # def point_original2normalized(x):
+    #     return (x - mean_pos) / std_pos
+
+    # def point_normalized2original(x):
+    #     return x * std_pos + mean_pos
+
+    # def distance_original2normalized(x):
+    #     return x / std_pos
+
+    # def distance_normalized2original(x):
+    #     return x * std_pos
+
+    # return point_original2normalized, point_normalized2original, distance_original2normalized, distance_normalized2original
+
+    # da3 is rougly metric and origin is in center so we just go from m to cm
     def point_original2normalized(x):
-        return (x - mean_pos) / std_pos
+        return x * 100
 
     def point_normalized2original(x):
-        return x * std_pos + mean_pos
+        return x / 100
 
     def distance_original2normalized(x):
-        return x / std_pos
+        return x * 100
 
     def distance_normalized2original(x):
-        return x * std_pos
+        return x / 100
 
     return point_original2normalized, point_normalized2original, distance_original2normalized, distance_normalized2original
